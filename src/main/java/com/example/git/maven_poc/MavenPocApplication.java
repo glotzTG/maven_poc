747a3b833f1a090fd3fe0ac3e594313f9c86632d
glotzTG <glotz@toolsgroup.com>
2024-04-22 15:52:21 -0600

    Initial Commit
    Initial Commit

---
+++ src/main/java/com/example/git/maven_poc/MavenPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.git.maven_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenPocApplication.class, args);
+	}
+
+}
